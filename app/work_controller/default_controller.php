7bb5ff66bd2a545a29f7666c1bb270f81a0220d7
bplilqd <bqlilpd@gmail.com>
2024-03-26 09:58:19 +0300

    add methods

--- app/work_controller/default_controller.php
+++ app/work_controller/default_controller.php
@@ -2,35 +2,44 @@
 
 namespace work;
 
-use model\default_model;
-use viwe\default_view;
-
 class default_controller extends main_controller
 {
 
     function __construct()
     {
+        // add name for set autoload for class
+        $this->start_name_class();
+        // set objects
+        $this->set_object();
+    }
+
+    // objects
+    protected function set_object()
+    {
+        $name_model = 'model\\' . NAME_MODEL;
+        $this->model = new $name_model;
+    }
+
+    // start name class
+    protected function start_name_class()
+    {
         // array for model class
-        $class_model = ['default_model'];
+        $class_model = [NAME_MODEL];
         // array for view class
         $class_view = ['default_view'];
         // autoload class
         $this->autoload_clas($class_model, $class_view);
-        // set objects
-        $this->viwe = new default_view;
-        $this->model = new default_model;
     }
 
-    // set autoload class
+    // set for autoload class
     protected function autoload_clas($class_model, $class_view)
     {
-        // set path
+        // set path for model
         $path_model = PATH . DS . 'app' . DS . 'class_model' . DS;
         $this->new_load_class($class_model, $path_model); // autoload for model
-        // set path
+        // set path for view
         $path_model = PATH . DS . 'app' . DS . 'page_view' . DS;
         $this->new_load_class($class_view, $path_model); // autoload for view
 
     }
-    
 }
