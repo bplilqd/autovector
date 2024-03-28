0601f9aa15e91108f1d76549a9a9ae2d86b17ef1
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-28 17:59:02 +0300

    those files was add  becaus was change name directory

---
+++ app/class_model/settings/config.php
@@ -0,0 +1,16 @@
+<?php
+// основные настройки
+//$root = $_SERVER['DOCUMENT_ROOT']; // site root
+
+//$mail_send = 'info@' . $_SERVER['SERVER_NAME']; // емайл сайта по умолчанию
+$mail_send = 'info@*****'; // емайл сайта по умолчанию
+$phone_default = '7921*****'; // формат 79214604140
+$secret_key = '*****'; // оригиналоьное значение менять тольео для нового приложения
+$set_cook_time_hash = 2592000;
+$design = 'page_iwe/template/design'; // папка с дизайном new_design/html
+$http_or_https = 'https://'; // http or https
+// подключение к базе данных
+$host_db = "localhost"; // хост
+$user_db = "autovector"; // пользователь
+$name_db = "autovector"; // имя базы данных
+$pass_db = "*****"; // пароль пользователя базы данных
