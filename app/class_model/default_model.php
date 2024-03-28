a8ad59f7adba9c7b38d03bb46f91e0ca7dfa216a
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-28 17:57:31 +0300

    set object user_config

--- app/class_model/default_model.php
+++ app/class_model/default_model.php
@@ -3,14 +3,30 @@
 namespace model;
 
 use viwe\default_view;
+use model\settings\user_config;
 
 class default_model extends model
 {
+    protected $user_config;
     protected $znach_array;
     public function __construct()
     {
         // set objects
+        $this->set_objects();
+        // имитируем запрос с базы данных
+        $this->query_data_user_db();
+    }
+
+    protected function set_objects()
+    {
+        $this->user_config = new user_config;
         $this->znach_array = new znach_array;
         $this->viwe = new default_view;
+    }
+
+    protected function query_data_user_db()
+    {
+        $array = ['user_theme' => 'dark_theme'];
+        $this->user_config->input_data($array);
     }
 }
