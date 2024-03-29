a94bb3e1589b8249e38c55b66962445f1dabaefb
Ivan Gerasimov <47751049+bplilqd@users.noreply.github.com>
2024-03-29 11:30:12 +0300

    update index.hpp

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ define("DS", DIRECTORY_SEPARATOR); // builds a file path with the appropriate di
 require_once PATH . DS . 'app' . DS . 'class_model' . DS . 'function' . DS . 'function.php'; // basic load function, config, class, constant and other...
 
 set_main_class(NAME_CONTROLLER); // set controller for start work
-$set_object_controller = 'controller\\'.NAME_CONTROLLER;
-$main = new $set_object_controller; // set object controller
+$main = new ('controller\\'.NAME_CONTROLLER); // set object controller
 
 print_r($main);
