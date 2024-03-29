84dced95b03f74b612e6920b5b47493fe481080a
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-29 22:27:57 +0300

    includeds templates

--- app/page_view/default_view.php
+++ app/page_view/default_view.php
@@ -3,5 +3,13 @@
 namespace viwe;
 
 class default_view extends viwe{
-    //
+    public function __construct()
+    {
+        $this->top = '<h1 class="text-info">Hello world!</h1>';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'header.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'top.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'content.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'sidebar.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'foot.html';
+    }
 }
