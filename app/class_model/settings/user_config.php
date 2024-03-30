50c95ac0fa7eae9e1a4256ece30332413f49e538
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-30 14:09:16 +0300

    add property

--- app/class_model/settings/user_config.php
+++ app/class_model/settings/user_config.php
@@ -5,6 +5,7 @@ namespace model\settings;
 class user_config implements interface_user_classe
 {
     public $user_theme; // name theme for use user
+    public $data_bs_theme;
     public $array; // array data
     public function input_data($array)
     {
@@ -14,5 +15,6 @@ class user_config implements interface_user_classe
     protected function set_data()
     {
         $this->user_theme = $this->array['user_theme']; // set name for the user theme
+        $this->data_bs_theme = $this->array['data_bs_theme']; // set for change dark or light
     }
 }
