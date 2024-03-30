2bd9004c4e7da021e0ec7dd82cb40f6d486d4a85
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-30 14:10:36 +0300

    added a require_once for includ new files of temlate

--- app/page_view/default_view.php
+++ app/page_view/default_view.php
@@ -2,14 +2,41 @@
 
 namespace viwe;
 
-class default_view extends viwe{
+interface set_theme
+{
+    public function __construct();
+    // print template
+    public function include_theme();
+}
+class default_view extends viwe implements set_theme
+{
     public function __construct()
     {
         $this->top = '<h1 class="text-info">Hello world!</h1>';
-        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'header.html';
-        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'top.html';
-        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'content.html';
-        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'sidebar.html';
-        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . 'dark_theme' . DS . 'foot.html';
+    }
+
+    public function include_theme()
+    {
+        // example structure
+        $arr = [
+            'header',
+            'menu',
+            'top',
+            'system_mesage',
+            'announce',
+            'title',
+            'content',
+            'sidebar',
+            'foot'
+        ];
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'header.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'menu.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'top.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'system_mesage.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'announce.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'title.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'content.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'sidebar.html';
+        require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . 'foot.html';
     }
 }
