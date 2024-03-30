adeb0682bc38330ea7042880d445049b70465f52
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-30 15:22:32 +0300

    correction

--- app/work_controller/auth_controller.php
+++ app/work_controller/auth_controller.php
@@ -7,24 +7,22 @@ class auth_controller extends main_controller
 
     function __construct()
     {
-        // обработка запросов пользователя
+        // обработка запросов пользователя check data input from user
 
         // add names for class for set autoload 
         $this->start_name_class();
         // set objects of model
         $this->set_object();
+        // sent data
+        $data=[];
+        $this->model->data_of_auth($data);
     }
 
     // start name class
     protected function start_name_class()
     {
-        // array for model -> settings class
-        $class_mosel_setings = ['interface_user_classe', 'user_config'];
-        $path_model = PATH . DS . 'app' . DS . 'class_model' . DS . 'settings' . DS;
-        $array[] = [$class_mosel_setings, $path_model];
-
         // array for model class
-        $class_model = [NAME_MODEL, 'znach_array'];
+        $class_model = [NAME_MODEL];
         $path_model = PATH . DS . 'app' . DS . 'class_model' . DS;
         $array[] = [$class_model, $path_model];
 
