d8b28137a89cfe3d06cb2a2c447c194020a51fb9
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-31 22:05:50 +0300

    added public method the user() for got data

--- app/class_model/settings/interface_user_classe.php
+++ app/class_model/settings/interface_user_classe.php
@@ -4,4 +4,5 @@ namespace model\settings;
 interface interface_user_classe
 {
     public function input_data($array); // set data of user from db
+    public function user(); // array with data of user
 }
--- app/class_model/settings/user_config.php
+++ app/class_model/settings/user_config.php
@@ -6,15 +6,22 @@ class user_config implements interface_user_classe
 {
     public $user_theme; // name theme for use user
     public $data_bs_theme;
+    public $id;
+    public $login;
+    public $name;
     public $array; // array data
     public function input_data($array)
     {
         $this->array = $array;
         $this->set_data();
     }
+    public function user(){
+        return $this->array;
+    }
     protected function set_data()
     {
         $this->user_theme = $this->array['user_theme']; // set name for the user theme
         $this->data_bs_theme = $this->array['data_bs_theme']; // set for change dark or light
     }
+    
 }
