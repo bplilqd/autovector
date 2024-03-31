a79bb2c78820c371abc16748ee00d8cc62516edb
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-31 18:52:32 +0300

    migration to main class useMysqli for mysql from forUseMysqli

--- app/class_model/connect/forUseMysqli.php
+++ app/class_model/connect/forUseMysqli.php
@@ -2,33 +2,15 @@
 
 namespace model\connect;
 
-class forUseMysqli implements interfaceForUseMysqli
+class forUseMysqli extends useMysqli implements interfaceForUseMysqli
 {
-    // for db
-    public $mysql = null;
-    public $query = null;
-    static $count_query; // count query
-    // handler of systeam messages
-    static $error_arr; // array with errors
-    static $success_arr; // array
-    static $warning_arr; // array
-    static $info_arr; // array info
-    // log query
-    static $registry_sql_echo; // array with the querys
-    // connect db
 
-    protected function set_db()
+    // error array
+    public function error_array()
     {
-        if (!$this->mysql) {
-            $this->mysql = new \mysqli(HOST_DB, USER_DB, PASS_DB, NAME_DB);
-            $this->mysql->set_charset("utf8");
-            // Проверка соединения
-            if ($this->mysql->connect_error) {
-                $this->error_arr[] = "Ошибка подключения к базе данных: " . $this->mysql->connect_error;
-            }
-        }
+        return $this->error_arr;
     }
-
+    
     // SELECT sql
     public function sql_select($sql_in, $echo = true, $no_log = false)
     {
@@ -84,20 +66,8 @@ class forUseMysqli implements interfaceForUseMysqli
         $this->registry_sql($sql, 'log_sql');
         return $result;
     }
-
-    // проверка на присутствие данных по задпнному запросу
-    protected function sql_true($echo = true)
-    {
-        // проверяем запрос
-        $result = ($this->query->num_rows >= 1) ? true : false;
-        if (!$result && $echo) {
-            $this->error_arr[] = 'По вашему запросу ничего не найдено!';
-        }
-        return $result;
-    }
-
     // соберает строку запроса для INSERT INTO
-    protected function insert_set_and_add($name_table, $array)
+    public function insert_set_and_add($name_table, $array)
     {
         $str = "INSERT INTO `$name_table` ";
         // подготовка ключи во значения в массив
@@ -118,27 +88,5 @@ class forUseMysqli implements interfaceForUseMysqli
         $str .= $str_values . ")";
         $result = $this->sql_insert($str);
         return $result;
-    }
-
-    protected function count_query_for_db($what_fun, $sql, $query_result = false)
-    {
-        $this->count_query[] = [
-            'sql_str' => $sql,
-            'fun' => $what_fun,
-            'time' => microtime(true),
-            'result' => $query_result
-        ];
-    }
-
-    // небольшой реестр пишет запросы в базу данных
-    private function registry_sql($sql, $dir = false)
-    {
-        //$str = date("d.m.Y H:i:s") . ' | ' . $sql . ' | ' . IN_URL . ' | ' . IP . ". \r\n";
-        $str = date("d.m.Y H:i:s") . ' | ' . $sql . ". \r\n";
-        $this->registry_sql_echo[] = $str;
-        // запись в файл
-        $new = fopen(PATH . DS . 'app' . DS . 'log' . DS . $dir . DS . "log_sql_" . date("m-Y") . ".txt", "a");
-        fwrite($new, $str);
-        fclose($new);
     }
 }
