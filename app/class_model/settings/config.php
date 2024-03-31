ecc590262b0bd41e8efc38d85a36922f033f0d9e
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-31 13:09:00 +0300

    update and add to .gitignore

--- app/class_model/settings/config.php
+++ app/class_model/settings/config.php
@@ -1,16 +1,10 @@
 <?php
 // основные настройки
-//$root = $_SERVER['DOCUMENT_ROOT']; // site root
-
-//$mail_send = 'info@' . $_SERVER['SERVER_NAME']; // емайл сайта по умолчанию
-$mail_send = 'info@*****'; // емайл сайта по умолчанию
-$phone_default = '7921*****'; // формат 79214604140
-$secret_key = '*****'; // оригиналоьное значение менять тольео для нового приложения
+$secret_key = '73a6015a9934aac8e4b566b076b54682'; // оригиналоьное значение менять тольео для нового приложения
 $set_cook_time_hash = 2592000;
-$design = 'page_iwe/template/design'; // папка с дизайном new_design/html
 $http_or_https = 'https://'; // http or https
 // подключение к базе данных
 $host_db = "localhost"; // хост
 $user_db = "autovector"; // пользователь
 $name_db = "autovector"; // имя базы данных
-$pass_db = "*****"; // пароль пользователя базы данных
+$pass_db = "72_B4TedH/r]VqHJ"; // пароль пользователя базы данных
