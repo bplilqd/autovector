b024d86a7a518c08d3ea7c1c1e1c7069f63f745d
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-31 19:58:56 +0300

    add for test img to conntent

--- app/page_view/default_view.php
+++ app/page_view/default_view.php
@@ -13,6 +13,9 @@ class default_view extends viwe implements set_theme
     public function __construct()
     {
         $this->top = '<h1 class="text-info">Hello world!</h1>';
+        $url_img_autovector = SITE_URL . 'images/autovector.jpg';
+        //$url_img_autovector = PATH . DS. 'images/autovector.jpg';
+        $this->content = '<img src="' . $url_img_autovector . '" class="img-fluid" alt="autovector">';
     }
 
     public function include_theme()
@@ -29,8 +32,8 @@ class default_view extends viwe implements set_theme
             'content',
             'foot'
         ];
-        foreach($array as $value){
-            require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . $value.'.html';
+        foreach ($array as $value) {
+            require_once PATH . DS . 'app' . DS . 'page_view' . DS . 'template' . DS . $this->user_theme . DS . $value . '.html';
         }
     }
 }
