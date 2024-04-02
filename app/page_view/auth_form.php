9f9587066a3b160466453f12a06b6c24911285d9
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-02 19:14:04 +0300

    continue set the form

--- app/page_view/auth_form.php
+++ app/page_view/auth_form.php
@@ -4,20 +4,45 @@ namespace view;
 
 class auth_form implements interface_auth_form
 {
+    protected $test;
     protected $array;
     protected $form;
-    public function input_data($array)
+    static $error_arr;
+
+    public function form($array)
     {
         $this->array = $array;
-    }
-    public function form()
-    {
         $this->set_form();
         return $this->form;
     }
     protected function set_form()
     {
-        $str = "form";
+        $disabled = '';
+        $active_input_pass = false;
+        if ($this->array['auth_form']['phone']) {
+            $disabled = ' disabled';
+            $active_input_pass = true;
+        }
+        $str = '
+    <form>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Телефон</label>
+            <input name="phone" value="' . $this->array['auth_form']['phone'] . '" type="phone" class="form-control" 
+            id="phone" aria-describedby="phoneHelp" placeholder="' . $this->array['auth_form']['phone'] . '"' . $disabled . '>
+            <div id="phoneHelp" class="form-text">Введите номер телефона на котором есть WhathsApp</div>
+        </div>';
+
+        if ($active_input_pass) {
+            $str .= '
+        <div class="mb-3">
+            <label for="pass" class="form-label">Пароль</label>
+            <input name="pass" type="password" class="form-control" id="pass">
+        </div>
+        ';
+        }
+        $str .= '
+        <button type="submit" name="auth_submit" class="btn btn-primary">Отправить</button>
+    </form>';
         $this->form = $str;
     }
 }
