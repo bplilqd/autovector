6ee0c708c968ab226319924798fd0e7eeb85cda4
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-04 21:03:24 +0300

    added variable $phone

--- app/page_view/auth_form.php
+++ app/page_view/auth_form.php
@@ -18,17 +18,21 @@ class auth_form implements interface_auth_form
     protected function set_form()
     {
         $disabled = '';
+        $phone = "";
         $active_input_pass = false;
-        if ($this->array['auth_form']['phone']) {
-            $disabled = ' disabled';
-            $active_input_pass = true;
+        if ($this->array) {
+            if ($this->array['auth_form']['phone']) {
+                $phone = $this->array['auth_form']['phone'];
+                $disabled = ' disabled';
+                $active_input_pass = true;
+            }
         }
         $str = '
     <form>
         <div class="mb-3">
             <label for="phone" class="form-label">Телефон</label>
-            <input name="phone" value="' . $this->array['auth_form']['phone'] . '" type="phone" class="form-control" 
-            id="phone" aria-describedby="phoneHelp" placeholder="' . $this->array['auth_form']['phone'] . '"' . $disabled . '>
+            <input name="phone" value="' . $phone . '" type="phone" class="form-control" 
+            id="phone" aria-describedby="phoneHelp" placeholder="' . $phone . '"' . $disabled . '>
             <div id="phoneHelp" class="form-text">Введите номер телефона на котором есть WhathsApp</div>
         </div>';
 
@@ -37,7 +41,7 @@ class auth_form implements interface_auth_form
         <div class="mb-3">
             <label for="pass" class="form-label">Пароль</label>
             <input name="pass" type="password" class="form-control" id="pass">
-            <input type="hidden" name="phone" value="' . $this->array['auth_form']['phone'] . '">
+            <input type="hidden" name="set_phone" value="' . $phone . '">
         </div>
         ';
         }
