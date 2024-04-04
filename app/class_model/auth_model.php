dc5b18c6943e0d4ba63bb985362c3d4e974237f3
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-04 20:54:07 +0300

    added registration new user with phone and generation password

--- app/class_model/auth_model.php
+++ app/class_model/auth_model.php
@@ -12,38 +12,65 @@ class auth_model extends model
     protected $auth_function;
     protected $auth_form;
     protected $input_data; // data for auth_form
+    protected $length_generate_pass = 8;
 
     public function __construct()
     {
         // set objects
         $this->set_objects();
 
-        // reg new user
-        //$this->registr_new_user();
-    }
-
-    public function data_of_auth($data)
-    {
-        $this->input_data = $data;
+        // example error for model
+        //$this->error_arr['model'][] = 'test error in the model';
     }
 
     public function set_and_settin_view()
     {
+        $data = $this->input_data;
+        if ($data) {
+            // registr new user
+            $this->set_registr($data);
+            // authorization
+            //...?
+        }
         // data transfer and set -> view
         $this->view->content = $this->auth_form->form($this->input_data);
         // default set to name of current theme
         $this->view->user_theme = DESIGN_THEME; // theme default
         // default set to what is the dark or light theme
         $this->view->data_bs_theme = MODE_THEME; // mode default
+        // got error from mysql
+        $this->error($this->mysql->error_arr, 'mysql');
         // include theme
         $this->view->include_theme();
     }
-    protected function registr_new_user()
+    public function data_of_auth($data)
     {
-        // check user in db
-        // ...?
-        $phone = 79214604140;
-        $pass = '123456qwerty';
+        $this->input_data = $data;
+        print_r($this->input_data);
+    }
+
+    protected function set_registr($data)
+    {
+        $phone = $data['auth_form']['phone'];
+        if ($data['auth_form']['auth_submit'] && $phone) {
+            $result = $this->check_user_to_db($phone);
+            if (!$result) {
+                $pass = $this->generateCode($this->length_generate_pass);
+                $this->registr_new_user($phone, $pass);
+                // уведомляем что отправили пароль
+            }
+        }
+    }
+    // is there a user in the database (db)
+    protected function check_user_to_db(int $phone)
+    {
+        $sql = "SELECT *  FROM `user` WHERE `phone` = $phone;";
+        $result = $this->mysql->sql_select($sql, false);
+        return $result;
+    }
+
+    protected function registr_new_user($phone, $pass)
+    {
         $generate_hash = $this->auth_function->create_md5_to_auth_phone(SECRET_KEY, $pass, $phone);
         $name_table = 'user';
         $array = [
@@ -58,9 +85,19 @@ class auth_model extends model
             'sec' => time()
         ];
         // add new user to db
+        print_r($array);
         $this->mysql->insert_set_and_add($name_table, $array);
     }
-
+    protected function generateCode($length = 6)
+    {
+        $chars = "abcdefikmprstuvwxyz123456789";
+        $code = "";
+        $clen = strlen($chars) - 1;
+        while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0, $clen)];
+        }
+        return $code;
+    }
     protected function set_objects()
     {
         // set object for connect mysql
