09f701904ed41f5119edc1c4bee1f2a775200992
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-05 18:49:35 +0300

    properties set

--- app/class_model/model.php
+++ app/class_model/model.php
@@ -5,7 +5,10 @@ namespace model;
 class model
 {
     protected $view;
-    static $error_arr; // error 
+    static $error_arr; // error
+    static $success_arr; // success
+    static $warning_arr; // warning
+    static $info_arr; // danger
 
     // collect errors
     public function error($array, $name)
