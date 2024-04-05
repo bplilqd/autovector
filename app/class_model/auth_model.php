4ed9fa38fee05fe68a8ec77aae42aee44e1d7a95
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-05 18:51:59 +0300

    updata

--- app/class_model/auth_model.php
+++ app/class_model/auth_model.php
@@ -8,11 +8,14 @@ use view\auth_form;
 
 class auth_model extends model
 {
+    // objects
     protected $mysql;
     protected $auth_function;
     protected $auth_form;
+    // other properties
     protected $input_data; // data for auth_form
     protected $length_generate_pass = 8;
+    protected $redirect = SITE_URL;
 
     public function __construct()
     {
@@ -25,6 +28,7 @@ class auth_model extends model
 
     public function set_and_settin_view()
     {
+        // receiving data
         $data = $this->input_data;
         // reg or auth
         $this->registr_or_authorization($data);
@@ -41,20 +45,24 @@ class auth_model extends model
         // include theme
         $this->view->include_theme();
     }
+
     protected function registr_or_authorization($data)
     {
         if ($data) {
             if ($data['auth_form']['auth_submit']) {
+
                 // registr new user
                 $phone = $data['auth_form']['phone'];
                 $this->set_registr($phone);
+
                 // authorization
                 $set_phone = $data['auth_form']['set_phone'];
                 $pass = $data['auth_form']['pass'];
-                $this->authorization($set_phone, $pass);
+                $this->set_authorization($set_phone, $pass);
             }
         }
     }
+
     protected function set_registr($phone)
     {
         if ($phone) {
@@ -67,24 +75,32 @@ class auth_model extends model
             }
         }
     }
-    protected function authorization($set_phone, $pass)
+
+    protected function authorization($set_phone, $pass_db, $generate_hash)
+    {
+        if ($pass_db && $pass_db == $generate_hash && !$this->error_arr['model']) {
+            // authorization good
+            $this->view->system_mesage = '-> authorization good';
+            //header("Location: $this->redirect");
+        } else {
+            // authorization error
+            $this->error_arr['model'][] = ' -> authorization error';
+            // redirect ?phone=$set_phone&auth_submit=auth_submit
+            //header("Location: ?phone=$set_phone&auth_submit=auth_submit");
+        }
+    }
+
+    protected function set_authorization($set_phone, $pass)
     {
         if ($set_phone && $pass) {
             $result = $this->check_user_to_db($set_phone);
-
             if ($result) {
+
                 $row = $this->mysql->query->fetch_assoc();
                 $pass_db = $row['pass'];
 
                 $generate_hash = $this->auth_function->create_md5_to_auth_phone(SECRET_KEY, $pass, $set_phone);
-                if ($pass_db && $pass_db == $generate_hash) {
-                    // authorization good
-                    $this->view->system_mesage = '-> authorization good';
-                    print_r('// authorization good');
-                } else {
-                    // authorization error
-                    print_r('// authorization error');
-                }
+                $this->authorization($set_phone, $pass_db, $generate_hash);
             }
         }
     }
