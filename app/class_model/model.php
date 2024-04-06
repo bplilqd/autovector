1c10f32c4fa404369f361ec119d6571eb3f0d065
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-06 16:09:23 +0300

    added methods set_user

--- app/class_model/model.php
+++ app/class_model/model.php
@@ -4,15 +4,41 @@ namespace model;
 
 class model
 {
-    protected $view;
+    // objects
+    protected object $mysql;
+    protected object $view;
+    protected object $user_config;
+
     static $error_arr; // error
     static $success_arr; // success
     static $warning_arr; // warning
     static $info_arr; // danger
 
-    // collect errors
+    public string $hash = ''; // id user of the hash
+    public bool $auth = false; // auth bool FALSE or TRUE
+
+    // collecter of errors
     public function error($array, $name)
     {
         $this->error_arr[$name] = $array;
+    }
+
+    public function set_user($hash)
+    {
+        if ($hash) {
+            $sql = "SELECT * FROM `user` WHERE `hash` = '$hash'";
+            // query
+            $result = $this->mysql->sql_select($sql, FALSE);
+            if ($result) {
+                $this->auth = TRUE;
+                $this->hash = $hash;
+                // got data of user
+                $row = $this->mysql->query->fetch_assoc();
+                // set user
+                $this->user_config->input_data($row);
+
+                // set online sec
+            }
+        }
     }
 }
