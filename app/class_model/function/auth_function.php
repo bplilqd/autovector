61f948648d7bb5df20302f295109cdfaf2ad63a5
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-14 21:56:33 +0300

    migration class from: app/work_controller/work/auth/auth_function.php to app/class_model/function/auth_function.php

---
+++ app/class_model/function/auth_function.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace model\function;
+
+class auth_function{
+
+    public $error_arr;
+    public function generateCode($length = 6)
+    {
+        $chars = "abcdefikmprstuvwxyz123456789";
+        $code = "";
+        $clen = strlen($chars) - 1;
+        while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0, $clen)];
+        }
+        return $code;
+    }
+    public function create_md5_to_auth_phone($secret_key, $pass, int $phone)
+    {
+        if ($secret_key && $pass && $phone) {
+            $generate_hash = md5("$secret_key:$pass:$phone");
+            return $generate_hash;
+        } else {
+            $this->error_arr[] = __FUNCTION__ . ' not data';
+        }
+    }
+}
