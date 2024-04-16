bf9029912afe2084d9d7f9d7a30d76e47c43ed76
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-16 17:16:20 +0300

    new file for lang: lang/en/auth.php and lang/ru/auth.php

---
+++ lang/en/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'not_correct_format_number' => 'Not correct format number phone, try example +79006003070 or 89006003070.'
+];
+
+//$auth = include 'lang/en/auth.php';
+
+//echo $auth['not_correct_format_number']; // Print: "Not correct format number phone, try example +79006003070 or 89006003070."
