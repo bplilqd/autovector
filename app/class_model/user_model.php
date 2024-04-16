13c15f2cf14245ac766e26bf68e385f7037a1503
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-16 11:23:19 +0300

    added method set_data_user_for_view()

--- app/class_model/user_model.php
+++ app/class_model/user_model.php
@@ -8,6 +8,8 @@ use model\connect\forUseMysqli;
 class user_model extends model
 {
     protected object $mysql;
+    // output data -> view
+    public $data_user;
 
     public function __construct()
     {
@@ -19,7 +21,8 @@ class user_model extends model
     {
         // if authorized. if allowed to continue
         if ($this->auth) {
-            
+            // data user for sent to wiew
+            $this->set_data_user_for_view();
         }
         // got error from mysql
         if ($this->mysql->error_arr) {
@@ -29,6 +32,22 @@ class user_model extends model
         if ($this->mysql->count_query) {
             $this->count_query = count($this->mysql->count_query);
         }
+    }
+
+    protected function set_data_user_for_view()
+    {
+        $user_config = $this->user_config;
+
+        $user_data = [
+            'name' => $user_config->name,
+            'phone' => $user_config->phone,
+            'email' => $user_config->email,
+            'user_theme' => $user_config->user_theme,
+            'data_bs_theme' => $user_config->data_bs_theme,
+            'date' => date("d.m.Y", $user_config->sec)
+        ];
+
+        $this->data_user = $user_data;
     }
 
     protected function set_objects()
