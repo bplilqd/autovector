6d2400e5cf8efc5c996defbd95d601a7dd021fe2
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-18 13:06:52 +0300

    update

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'not_correct_format_number' => 'Not correct format number phone, try example +79006003070 or 89006003070.',
-    'enter_phone' => 'Enter phone number.',
+    'not_correct_format_number' => 'Wrong number, try in the format +79006003070 or 89006003070.',
+    'enter_phone' => 'Enter your phone number.',
     'redirection_after' => 'You are already logged in, you will be redirected in 5 seconds.',
     'no_pass' => 'No password. Enter your password.',
     'incorrect_pass' => 'Incorrect password.',
     'not_authorized' => 'Not authorized, there will be a redirection after 5 seconds',
-    'error_captcha' => 'Error of recaptcha.',
+    'error_captcha' => 'Recaptcha error.',
     'verification_failed' => 'Verification failed, click - I\'m not a robot.'
 ];
