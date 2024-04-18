358777bf0e883ba48292b148140bc653901818c1
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-18 13:07:13 +0300

    new file

---
+++ lang/en/auth_form.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'phone' => 'Telephone',
+    'pass' => 'Password',
+    'help_text' => 'Enter the phone number that has WhathsApp',
+    'help_text2' => 'Enter ',
+    'help_text3' => 'another number',
+    'auth_submit' => 'Submit'
+];
