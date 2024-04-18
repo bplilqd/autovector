43c319cf167ca2c4fd392155bb663828c6e873ad
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-18 14:25:57 +0300

    new file

---
+++ lang/en/panel_user.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title_user_page' => 'User page',
+    'profile' => 'Profile',
+    'settings' => 'Settings',
+    'logout' => 'Exit'
+];
---
+++ lang/ru/panel_user.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title_user_page' => 'Страница пользователя',
+    'profile' => 'Профиль',
+    'settings' => 'Настройки',
+    'logout' => 'Выход'
+];
