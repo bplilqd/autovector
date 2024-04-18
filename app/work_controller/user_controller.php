e0d7193ec5401e4b385e67e5f5cddb8be7d90a3b
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-18 19:08:49 +0300

    set znach_array

--- app/work_controller/user_controller.php
+++ app/work_controller/user_controller.php
@@ -92,7 +92,12 @@ class user_controller extends main_controller
         $class_mosel_setings = ['interfaceForUseMysqli', 'useMysqli', 'forUseMysqli'];
         $path_model = PATH . DS . 'app' . DS . 'class_model' . DS . 'connect' . DS;
         $array[] = [$class_mosel_setings, $path_model];
-
+        
+        // array for model -> function class
+        $class_mosel_setings = ['znach_array'];
+        $path_model = PATH . DS . 'app' . DS . 'class_model' . DS . 'function' . DS;
+        $array[] = [$class_mosel_setings, $path_model];
+        
         // array for model -> settings class
         $class_mosel_setings = ['interface_user_classe', 'user_config'];
         $path_model = PATH . DS . 'app' . DS . 'class_model' . DS . 'settings' . DS;
