1103058beea439e1bcf96d82feca4f5910c71c47
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-21 14:49:45 +0300

    update

--- app/work_controller/user_controller.php
+++ app/work_controller/user_controller.php
@@ -100,6 +100,7 @@ class user_controller extends main_controller
                         )
                     );
                 }
+                // logged out
                 $this->go_out();
             }
         }
