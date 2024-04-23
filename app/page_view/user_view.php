67a0d0cbd88448ea4565569aacd1bc2373a1e46c
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-23 19:45:09 +0300

    added a button to go to editing

--- app/page_view/user_view.php
+++ app/page_view/user_view.php
@@ -2,19 +2,20 @@
 
 namespace view;
 
-use model\function\znach_array;
+//use model\function\znach_array;
 
 class user_view extends view implements interface_auth_view, interface_user_view
 {
   protected $data_user;
   protected $array_info_user_content;
 
-  protected $znach_array;
+  //protected $znach_array;
 
   public function __construct()
   {
     // set
-    $this->znach_array = new znach_array;
+    //$this->znach_array = new znach_array;
+  
     // start standart
     $this->start_standart_view();
     // data output limitation
@@ -37,9 +38,9 @@ class user_view extends view implements interface_auth_view, interface_user_view
         <ul class="nav nav-underline">';
 
     foreach ($array as $value) {
-    $menu .= '
+      $menu .= '
           <li class="nav-item">
-            <a class="nav-link '.$value[0].'" href="'.$value[1].'">' . $this->translations->get_message('panel_user', $value[2]) . '</a>
+            <a class="nav-link ' . $value[0] . '" href="' . $value[1] . '">' . $this->translations->get_message('panel_user', $value[2]) . '</a>
           </li>';
     }
 
@@ -53,20 +54,23 @@ class user_view extends view implements interface_auth_view, interface_user_view
   {
     $data = $this->data_user;
     $content = '
-<ul class="list-group list-group-flush">
-';
+<ul class="list-group list-group-flush">';
     $array = $this->array_info_user_content;
     foreach ($data as $key => $value) {
       // if it's on the list
       if ($value && in_array($key, $array)) {
         $content .= '
-    <li class="list-group-item">' . $value . '</li>
-    ';
+    <li class="list-group-item">' . $value . '</li>';
       }
     }
     $content .= '
-</ul>
-    ';
+    <li class="list-group-item"><a class="btn btn-secondary" href="?edit_user" role="button">' .
+      $this->translations->get_message(
+        'panel_user',
+        'edit'
+      ) . '</a></li>';
+    $content .= '
+</ul>';
     $this->setting_properties('content', $content);
   }
 
