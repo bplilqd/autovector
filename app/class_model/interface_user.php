f1b78e5cd4db09a48d4e812511655644072814ee
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-23 19:43:14 +0300

    added interface_user -> for model

---
+++ app/class_model/interface_user.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace model;
+
+interface interface_user
+{
+    // set user
+    public function set_user($hash);
+    // method work of class
+    public function set_and_setting();
+    // setting form
+    public function edit_form($data);
+}
