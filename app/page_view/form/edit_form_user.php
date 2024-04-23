8bb086b5b4cc7d80467ded999c86624cd0654d8a
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-23 19:46:45 +0300

    set_form() update

--- app/page_view/form/edit_form_user.php
+++ app/page_view/form/edit_form_user.php
@@ -18,9 +18,31 @@ class edit_form_user extends form implements interface_form
         // sent
         return $this->form;
     }
+
     protected function set_form()
     {
-        $str = '';
+        $date = $this->date;
+        $name = $date['edit_form']['name'] ? $date['edit_form']['name'] : $date['user_config']['name'];
+        $last_name  = $date['edit_form']['last_name'] ? $date['edit_form']['last_name'] : $date['user_config']['last_name'];
+        
+        $name_word = $this->translations->get_message('panel_user', 'name');
+        $last_name_word = $this->translations->get_message('panel_user', 'last_name');
+        $str = '
+      <form>
+        <div class="row g-3">
+          <div class="col">
+            <input type="hidden" name="edit_user">
+            <input type="text" name="name" value="' . $name . '" class="form-control" placeholder="' . $name_word . '" aria-label="' . $name_word . '">
+          </div>
+          <div class="col">
+            <input type="text" name="last_name" value="' . $last_name . '" class="form-control" placeholder="' . $last_name_word . '"
+              aria-label="' . $last_name_word . '">
+          </div>
+          <div class="col-12">
+            <button type="submit" name="submit_edit" class="btn btn-primary">Сохранить</button>
+          </div>
+        </div>
+      </form>';
         $this->form = $str;
     }
 }
