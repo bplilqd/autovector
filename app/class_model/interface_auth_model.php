d372954fa1ee46d93579690945cc9b1fb5e5b853
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-23 19:42:27 +0300

    update

--- app/class_model/interface_auth_model.php
+++ app/class_model/interface_auth_model.php
@@ -4,10 +4,10 @@ namespace model;
 
 interface interface_auth_model
 {
+    // set user
+    public function set_user($hash);
     // method work of class
     public function set_and_setting();
     // data of user got from form
     public function data_of_auth($data);
-    // set user
-    public function set_user($hash);
 }
