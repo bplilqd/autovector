163c87bf2e94f969024c7c5a7fa555c54c820782
Ivan Gerasimov <bqlilpd@gmail.com>
2024-04-24 20:19:31 +0300

    off test error

--- panel/user/index.php
+++ panel/user/index.php
@@ -1,6 +1,6 @@
 <?php
- error_reporting(E_ALL);
- ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
 $name_model = 'user_model'; // name basic of model / NAME_MODEL
 $name_controller = 'user_controller'; // name basic of controller / NAME_CONTROLLER
