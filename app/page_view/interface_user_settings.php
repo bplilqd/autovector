550fe7e835682b9b35b6f46eba4c2c9f967c0b6d
Ivan Gerasimov <bqlilpd@gmail.com>
2024-05-04 15:31:59 +0300

    added new interface interface_user_settings

---
+++ app/page_view/interface_user_settings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace view;
+
+interface interface_user_settings
+{
+    // set menu
+    public function set_menu();
+    // input data
+    public function input_data_user($data_user);
+}
