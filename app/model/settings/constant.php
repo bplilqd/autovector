8a4a797f894bd07b3d0987df25ec5f56f567e246
Ivan Gerasimov <bqlilpd@gmail.com>
2024-06-07 22:56:17 +0300

    added DIR_APP and change SITE_URL

--- app/model/settings/constant.php
+++ app/model/settings/constant.php
@@ -17,6 +17,7 @@ define("LANGUAGE", 'en'); // mode default
 define("NAME_MODEL", $name_model); // name basic of model
 define("NAME_CONTROLLER", $name_controller); // name basic of controller
 define("NAME_VIEW", $name_view); // name basic ofview / NAME_VIEW
+define('DIR_APP', $dir_app . DS); // директория сайта, приложения
 
 // служебные
 define("USER_HASH", $_COOKIE['hash']); // hash
@@ -24,7 +25,7 @@ define("IP", $_SERVER['REMOTE_ADDR']); // ip адрес пользователя
 define("HOST", $_SERVER['SERVER_NAME']); // имя сайта
 define("REFERER", $_SERVER['HTTP_REFERER']); // реф ссылка
 define("HTTP_HTTPS", $http_or_https); // протокол по умолчанию например https://
-define("SITE_URL", HTTP_HTTPS . HOST . DS);
+define("SITE_URL", HTTP_HTTPS . HOST . DS . DIR_APP);
 define("MOBILE_DEVICE", $mobile_device_true);
 define("SECRET_KEY", $secret_key); // секретный ключ для генерации md5
 define("SET_COOK_TIME_HASH", $set_cook_time_hash); // установка времени жизни куки для авторизации
