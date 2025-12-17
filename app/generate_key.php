ba096b9e5810278756a764dd99f4af4ffa389ab9
Ivan Gerasimov <pqlilpd@gmail.com>
2025-12-17 09:32:28 +0300

    updated

--- app/generate_key.php
+++ app/generate_key.php
@@ -5,6 +5,5 @@ include_once __DIR__ . '/model/function/generateCustomKey.php';
 use function model\function\generateCustomKey;
 
 // Пример использования для генерации ключа длиной 32 символа
-$key = generateCustomKey();
-echo $key;
+echo generateCustomKey();
 // Пример вывода: GFGl4756hdkcnfDSFTO4793dhf454kkK
